8660ca90b7e25e714ad68188f93f7f1a6a776e70
putra2016 <skputra2016@gmail.com>
2024-04-01 19:30:49 +0700

    Update prod.exs

--- uploadfile/config/prod.exs
+++ uploadfile/config/prod.exs
@@ -2,7 +2,7 @@ import Config
 
 # For production, don't forget to configure the url host
 # to something meaningful, Phoenix uses this information
-# when generating URLs.
+# when generating URLs
 
 # Notes we also include the path to a cache manifest
 # containing the digested version of static files. This
